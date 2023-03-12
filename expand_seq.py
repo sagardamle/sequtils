7bbaec61d831fc3ea21cf0818495b901c9633dcb
sagardamle <127635374+sagardamle@users.noreply.github.com>
2023-03-11 16:59:22 -0800

    Update expand_seq.py

--- expand_seq.py
+++ expand_seq.py
@@ -1,4 +1,5 @@
 from itertools import combinations, product
+from collections import defaultdict
 
 def edit_expand(seq, edits = 2, ins = True, alphabet = ['A', 'G', 'C', 'T']):
     """ Compute the shell of related sequences to seed
@@ -9,28 +10,30 @@ def edit_expand(seq, edits = 2, ins = True, alphabet = ['A', 'G', 'C', 'T']):
     Returns:
     generator of neighbor sequences
     """
-    insalphabet_dict = {l: [] for l in alphabet}
-    seq = seq
     seqlist = list(seq)
+    insalphabet_dict = defaultdict(list)
+    
     if ins:
-        for nuc in alphabet + [' ']:
-            insalphabet = [l + nuc for l in alphabet]
-            insalphabet += ['']
-            insalphabet_dict[nuc] = insalphabet
+        for nuc in alphabet:
+            insalphabet_dict[nuc] += [l + nuc for l in alphabet] + ['']
     
     for d in range(edits + 1):
         for locs in combinations(range(len(seq)), d):
             thisseq = seqlist.copy()
             for loc in locs:
                 origchar = seq[loc]
-                if loc:
-                    thisseq[loc] = [l for l in alphabet + insalphabet_dict[origchar] if l != origchar]
+                if loc == 0:
+                    if ins:
+                        thisseq[loc] = [l for l in alphabet + [''] if l != origchar]
+                    else:
+                        thisseq[loc] = [l for l in alphabet if l != origchar]
                 else:
-                    thisseq[loc] = [l for l in alphabet + [''] if l != origchar]
+                    thisseq[loc] = [l for l in alphabet + insalphabet_dict[origchar] if l != origchar]
+                    
             for poss in product(*thisseq):
                 yield "".join(poss)
 
 def expand_sequences(*args, **kwargs):
     return list(set(filter(None, edit_expand(*args, **kwargs))))
 
-results = expand_sequences('GT', edits = 1)
+gg = expand_sequences('GTC', edits = 1, ins = False)
